9f256a5ea1c426a5a6c3b6ebbcf5afd7e289b0b0
Ed <ed@monepic.com>
2020-06-25 00:49:58 +0100

    Add kafka-console-consumer.sh autocomplete

---
+++ helpers.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+_get_value() {
+    local option="$1"
+    shift
+    local array=("$@")
+    local foundOption=false
+
+    for element in "${array[@]}"; do
+        $foundOption && {
+            [[ "$element" == "=" ]] && continue # ignore equals in --key=value syntax
+                    echo "$element"
+                    return 
+                }
+            [[ "$element" == "$option" ]] && { foundOption=true; }
+        done
+    }
+
+_get_bootstrap_server() {
+    # see if we've a bootstrap server value in the argument list
+    local bs=$(_get_value --bootstrap-server "${COMP_WORDS[@]}")
+
+    # check we've received something and it's a credible host:port otherwise try the env variable
+    if [[ "$bs" =~ [^[:space:]]*:[0-9]+$ ]]; then
+      echo "$bs"
+    else
+      echo "$KAFKA_BOOTSTRAP_SERVER"
+   fi
+}
+
+_get_topics() {
+    # if we've found a bootstrap server, query the topics
+    local bs=$(_get_bootstrap_server)
+    if [[ ! -z "$bs" ]]; then
+        kafka-topics.sh --bootstrap-server "$bs" --list 2>/dev/null
+    fi
+}
+
+_get_consumer_groups() {
+    # if we've found a bootstrap server, query the groups
+    local bs=$(_get_bootstrap_server)
+    if [[ ! -z "$bs" ]]; then
+        kafka-consumer-groups.sh --bootstrap-server "$bs" --list 
+    fi
+}
